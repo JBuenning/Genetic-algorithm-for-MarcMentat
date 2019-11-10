414bbc93630b6423e0b883441133472dd80dc140
tobias.knieps <tobias.knieps@gmail.com>
2019-11-10 23:28:49 +0100

    Anzeigen eines Polygons mit matplotlib

--- GUI.py
+++ GUI.py
@@ -1,8 +1,23 @@
 import tkinter as tk
+import matplotlib.pyplot as plt
+import shapely
+from shapely.geometry import Polygon
 
+### Initalisieren des Fensters ###
 root = tk.Tk()
 frame = tk.Frame(root, bg = 'black')
 canvas = tk.Canvas(root, height=500, width=500)
 canvas.pack()
+
+def show_polygon():
+    pass
+
+def create_example_polygon():
+    return Polygon([(0,0),(2,2),(2,1),(1,0)])
+
+example = create_example_polygon()
+
+plt.plot(*example.exterior.xy, marker = 'o')
+plt.show()
 
 root.mainloop()
