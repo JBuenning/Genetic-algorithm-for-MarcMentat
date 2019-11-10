a267235eb632d70968ee3bb422bde24591089c32
Jonas Bünning <jonas.buenning@t-online.de>
2019-11-10 21:54:06 +0000

    add documentation

--- shape.py
+++ shape.py
@@ -1,4 +1,17 @@
 from shapely import geometry
 
 class Shape(geometry.Polygon):
-    pass
+    def __init__(self, shell, holes=None, fixed_x=None, fixed_y=None):
+        super().__init__(shell, holes)
+        self.fixed_x = fixed_x
+        self.fixed_y = fixed_y
+
+    #Methoden und Attribute, die schon da sind:
+        
+    #object.exterior.coords -> Liste mit äußeren Koordinaten [(x,y),(x,y)...]
+    #object.interior.coords -> innere Koordinaten[[(x,y),(x,y)...],[...],...]
+    #object.is_valid -> True wenn valid
+    #object.is_simple -> True wenn keine Überschneidung mit sich selbst
+    #object.area
+    #...
+    
