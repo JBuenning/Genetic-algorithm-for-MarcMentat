1f4de88421100c8b9c73051569043265d6a43722
Jonas Bünning <jonas.buenning@t-online.de>
2019-11-14 16:04:09 +0000

    restriction hinzugefuegt

--- shape.py
+++ shape.py
@@ -2,20 +2,35 @@ from shapely import geometry
 import matplotlib.pyplot as plt
 import numpy as np
 
-def evened_out_point(point, neighbour1, neighbour2):
+def evened_out_point(point, neighbour1, neighbour2, restriction=False):
     px, py = point
     n1x, n1y = neighbour1
     n2x, n2y = neighbour2
     mx = (n2x + n1x)/2
     my = (n2y + n1y)/2
+    
+    if restriction:
+        restriction_x, restriction_y = restriction
+        #in Matrixschreibweise AX=B
+        try:
+            A = np.array([[restriction_x, n1y-n2y],[restriction_y, n2x-n1x]])
+            B = np.array([mx-px, my-py])
+            t,u = np.linalg.solve(A,B)
 
-    #in Matrixschreibweise AX=B
-    A = np.array([[n2x-n1x, n1y-n2y],[n2y-n1y, n2x-n1x]])
-    B = np.array([mx-px, my-py])
-    t,u = np.linalg.solve(A,B)
+            x = px + t*(restriction_x)
+            y = py + t*(restriction_y)
+        except:
+            #wenn der Punkt sich wegen der restriction nur senkrecht zu den Nachbarpunkten bewegen dürfte
+            x, y = point
 
-    x = px + t*(n2x - n1x)
-    y = py + t*(n2y - n1y)
+    else:
+        #in Matrixschreibweise AX=B
+        A = np.array([[n2x-n1x, n1y-n2y],[n2y-n1y, n2x-n1x]])
+        B = np.array([mx-px, my-py])
+        t,u = np.linalg.solve(A,B)
+
+        x = px + t*(n2x - n1x)
+        y = py + t*(n2y - n1y)
     return(x,y)
 
 class Shape(geometry.Polygon):
@@ -66,7 +81,7 @@ class Shape(geometry.Polygon):
 
 
 if __name__=='__main__':
-    print(evened_out_point((1,4), (0,0), (10,0)))
+    print(evened_out_point((1,4), (0,0), (10,0), restriction=(0,1)))
 
 
 
