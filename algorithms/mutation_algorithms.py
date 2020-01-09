bf40773809e2063d139121f787d3932d380a3acb
Jonas Bünning <jonas.buenning@t-online.de>
2020-01-09 16:58:50 +0000

    first changes

--- algorithms/mutation_algorithms.py
+++ algorithms/mutation_algorithms.py
@@ -10,7 +10,7 @@ def get_all_mutation_algorithms():
     Returns:
         list: Contains one object of evers mutation algorithm
     """
-    lst = [AlgorithmOne()]
+    lst = [AlgorithmOne(), AlgorithmTwo()]
     return lst
 
 class MutationAlgorithm(algorithm.Algorithm):
@@ -157,6 +157,113 @@ class AlgorithmOne(MutationAlgorithm):
 
     def get_name(self):
         return 'Algorithm One'
+
+class AlgorithmTwo(MutationAlgorithm):
+
+    def get_name(self):
+        return 'Algorithm two'
+
+    def default_settings(self):
+        self.min_movement = 1.0
+        self.max_movement = 100.0
+        self.min_radius = 0.0 #in this case like Algorithm one
+        self.max_radius = 20.0
+        self.max_recursiondepth=20
+        self.n_times = 20
+
+    def set_default(self):# erklären lassen von jonas
+        self.default_settings()
+        self.ent_min_movement.delete(0,'end')
+        self.ent_min_movement.insert(0, self.min_movement)
+        self.ent_max_movement.delete(0,'end')
+        self.ent_max_movement.insert(0, self.max_movement)
+        self.ent_min_radius.delete(0, 'end')
+        self.ent_min_radius.insert(0, self.min_radius)
+        self.ent_max_radius.delete(0, 'end')
+        self.ent_max_radius.insert(0, self.max_radius)
+        self.ent_max_recursiondepth.delete(0,'end')
+        self.ent_max_recursiondepth.insert(0, self.max_recursiondepth)
+        self.ent_n_times.delete(0, 'end')
+        self.ent_n_times.insert(0, self.n_times)
+
+    def apply(self):
+        try:
+            self.min_movement = float(self.ent_min_movement.get())
+            self.ent_min_movement.delete(0, 'end')
+            self.ent_min_movement.insert(0, self.min_movement)
+        except:
+            self.ent_min_movement.delete(0, 'end')
+            self.ent_min_movement.insert(0, self.min_movement)
+        try:
+            self.max_movement = float(self.ent_max_movement.get())
+            self.ent_max_movement.delete(0, 'end')
+            self.ent_max_movement.insert(0, self.max_movement)
+        except:
+            self.ent_max_movement.delete(0, 'end')
+            self.ent_max_movement.insert(0, self.max_movement)
+
+        try:
+            self.min_radius = float(self.ent_min_radius.get())
+            self.ent_min_radius.delete(0, 'end')
+            self.ent_min_radius.insert(0, self.min_radius)
+        except:
+            self.ent_min_radius.delete(0, 'end')
+            self.ent_min_radius.insert(0, self.min_radius)
+
+        try:
+            self.max_radius = float(self.ent_max_radius.get())
+            self.ent_max_radius.delete(0, 'end')
+            self.ent_max_radius.insert(0, self.max_radius)
+        except:
+            self.ent_max_radius.delete(0, 'end')
+            self.ent_max_radius.insert(0, self.max_radius)
+        try:
+            self.max_recursiondepth = int(self.ent_max_recursiondepth.get())
+            self.ent_max_recursiondepth.delete(0, 'end')
+            self.ent_max_recursiondepth.insert(0, self.max_recursiondepth)
+        except:
+            self.ent_max_recursiondepth.delete(0, 'end')
+            self.ent_max_recursiondepth.insert(0, self.max_recursiondepth)
+        try:
+            self.n_times = int(self.ent_n_times.get())
+            self.ent_n_times.delete(0, 'end')
+            self.ent_n_times.insert(0, self.n_times)
+        except:
+            self.ent_n_times.delete(0, 'end')
+            self.ent_n_times.insert(0, self.n_times)
+
+    def get_settings_frame(self, master):
+        frame = tk.Frame(master)
+        tk.Label(frame, text='min movement').grid(row=0, column=0, sticky='w')
+        self.ent_min_movement = tk.Entry(frame, width=5)
+        self.ent_min_movement.grid(row=0, column=1)
+        tk.Label(frame, text='max movement').grid(row=1, column=0, sticky='w')
+        self.ent_max_movement = tk.Entry(frame, width=5)
+        self.ent_max_movement.grid(row=1, column=1)
+
+        tk.Label(frame, text='min radius').grid(row=2, column=0, sticky='w')
+        self.ent_min_radius = tk.Entry(frame, width=5)
+        self.ent_min_radius.grid(row=2, column=1)
+
+        tk.Label(frame, text='max radius').grid(row=3, column=0, sticky='w')
+        self.ent_max_radius = tk.Entry(frame, width=5)
+        self.ent_max_radius.grid(row=3, column=1)
+
+        tk.Label(frame, text='max recursion depth').grid(row=4, column=0, sticky='w')
+        self.ent_max_recursiondepth = tk.Entry(frame, width=5)
+        self.ent_max_recursiondepth.grid(row=4, column=1)
+        tk.Label(frame, text='repetitions').grid(row=5, column=0, sticky='w')
+        self.ent_n_times = tk.Entry(frame, width=5)
+        self.ent_n_times.grid(row=5, column=1)
+        apply = ttk.Button(frame, text='apply', command=self.apply)
+        apply.grid(row=6, column=0, pady=4)
+        default = ttk.Button(frame, text='reset', command=self.set_default)
+        default.grid(row=6, column=1, pady=4)
+        self.set_default()
+        return frame
+
+    def change_shape(self, shp):
+        pass
 
     
     
